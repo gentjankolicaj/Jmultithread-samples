da8e32cdc4dc8142162948a87319ad2ff84cdcb2
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-02-14 16:31:21 +0100

    second  commit.

---
+++ src/com/section/tut_2/App1.java
@@ -0,0 +1,44 @@
+package com.section.tut_2;
+
+import java.util.Scanner;
+
+class Processor extends Thread {
+	
+	private volatile boolean  running=true; //volatile used to prevent threads from caching variables when they are not changed by that thread
+	
+	public void run() {
+		System.out.println("Inside infinite loop.");
+		while(running) {
+			
+			try {
+				
+				Thread.sleep(2000);
+				System.out.println("infinite loop.");
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			}
+		}
+	}
+	
+	public void shutdown() {
+		running=false;
+	}
+	
+}
+public class App1 {
+
+	public static void main(String[] args) {
+		Processor proc=new Processor();
+		proc.start();
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Press to shudown...");
+		scanner.nextLine();
+		proc.shutdown();
+
+	}
+
+}
